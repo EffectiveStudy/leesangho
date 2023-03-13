61b2f812b6efbcf9d995b242aa1b385002bf517e
sangho lee <sangholee.dev@gmail.com>
2023-03-13 21:22:28 +0900

    feat: study 열한번째 item57 추가

---
+++ src/main/java/com/github/sangholee/dev/effectivejavastudy/study11_item57/Item57.java
@@ -0,0 +1,45 @@
+package com.github.sangholee.dev.effectivejavastudy.study11_item57;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import javax.lang.model.element.Element;
+
+public class Item57 {
+
+    public static void main(String[] args) {
+        caseWhile();
+        caseFor();
+    }
+
+    private static void caseWhile() {
+        List<Element> c = new ArrayList<>();
+        List<Element> c2 = new ArrayList<>();
+        Iterator<Element> i = c.iterator();
+        while(i.hasNext()) {
+            doSomeThing(i.next());
+        }
+
+        Iterator<Element> i2 = c2.iterator();
+        while(i.hasNext()) { // 복붙으로 인한 오류
+            doSomeThing(i2.next());
+        }
+    }
+
+    private static void doSomeThing(Element next) {
+        System.out.println(next);
+    }
+
+    private static void caseFor() {
+        List<Element> c = new ArrayList<>();
+        List<Element> c2 = new ArrayList<>();
+
+        for(Iterator<Element> i = c.iterator(); i.hasNext();) {
+            Element e = i.next();
+        }
+
+        for(Iterator<Element> i2 = c2.iterator(); i.hasNext();) {
+            Element e2 = i2.next();
+        }
+    }
+}
